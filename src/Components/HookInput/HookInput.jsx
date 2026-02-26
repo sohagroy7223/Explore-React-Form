9cc4a35eace5c901781e7bdcad1d0e16134eb59d
sohagroy7223 <freelancersohagroy@gmail.com>
2026-02-26 13:26:04 +0600

    adad email and password in use coustom hook useInputFile

--- src/Components/HookInput/HookInput.jsx
+++ src/Components/HookInput/HookInput.jsx
@@ -3,16 +3,33 @@ import useInputFile from "../../Hooks/UseInputFile";
 
 const HookInput = () => {
   const [name, changeName] = useInputFile("");
+  const [email, changeEmail] = useInputFile("");
+  const [password, changePassword] = useInputFile("");
   const handelSubmit = (e) => {
     e.preventDefault();
 
-    console.log("hay", name);
+    console.log(name, email, password);
   };
   return (
     <div>
       <form onSubmit={handelSubmit}>
         <input type="text" onChange={changeName} placeholder="your name" />
         <br />
+        <input
+          type="email"
+          onChange={changeEmail}
+          name="email"
+          placeholder="email"
+        />
+        <br />
+        <input
+          type="password"
+          onChange={changePassword}
+          name="password"
+          placeholder="password"
+        />
+        <br />
+
         <input type="submit" value="submit" />
       </form>
     </div>
