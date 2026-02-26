46ddade553e1557592e84e769766e869902a6d5a
sohagroy7223 <freelancersohagroy@gmail.com>
2026-02-26 23:55:08 +0600

    addad context to pass the value in any property

--- src/Components/FamilyTree/FamilyTree.jsx
+++ src/Components/FamilyTree/FamilyTree.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { createContext } from "react";
 import Grandpa from "./Grandpa";
 import "./FamilyTree.css";
 
+export const AssetContext = createContext("");
+
 const FamilyTree = () => {
   const asset = "diamond";
+  const newAsset = "Gold";
 
   return (
     <div className="family-tree">
       <h2>Family Tree</h2>
-      <Grandpa asset={asset}></Grandpa>
+      <AssetContext.Provider value={newAsset}>
+        <Grandpa asset={asset}></Grandpa>
+      </AssetContext.Provider>
     </div>
   );
 };
--- src/Components/FamilyTree/Spacial.jsx
+++ src/Components/FamilyTree/Spacial.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AssetContext } from "./FamilyTree";
 
 const Spacial = ({ asset }) => {
+  const newAsset = useContext(AssetContext);
+  console.log("new asset", newAsset);
   return (
     <div>
       <h3>Spacial</h3>
       <p>asset: {asset} </p>
+      <p>mew asset: {newAsset}</p>
     </div>
   );
 };
