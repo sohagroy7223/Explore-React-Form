a86e15788dac4a4587626d331b273eb2136b118b
sohagroy7223 <freelancersohagroy@gmail.com>
2026-02-27 13:01:35 +0600

    addad multyple useState in this context

--- src/Components/FamilyTree/UncleSons.jsx
+++ src/Components/FamilyTree/UncleSons.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { use } from "react";
+import { MoneyContext } from "./FamilyTree";
 
 const UncleSons = ({ name }) => {
+  const [money, setMoney] = use(MoneyContext);
+
+  const handelAdd = () => {
+    setMoney(money + 2000);
+  };
   return (
     <div>
       <h3>{name}</h3>
+      {name === "Akash" && <button onClick={handelAdd}>add 2000 </button>}
     </div>
   );
 };
